9ecd26d07656a9466b5db41b72cbbe0e57890df9
Michel Sterling <michel.sterling@gmail.com>
2025-11-08 22:15:58 -0600

    added flip

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import shapes from './assets/shapes.json';
 
@@ -6,35 +6,60 @@ interface Shape {
   name: string;
 }
 
-const ShapeComponent = ({ name }: { name: string }) => {
-  const size = 75; // Size of the shape in pixels
-
-  switch (name) {
-    case 'Triangle':
-      return (
-        <svg width={size} height={size} viewBox="0 0 50 50">
-          <polygon points="25,5 45,45 5,45" stroke="currentColor" fill="none" />
-        </svg>
-      );
-    case 'Square':
-      return (
-        <svg width={size} height={size} viewBox="0 0 60 60">
-          <rect x="5" y="5" width="50" height="50" stroke="currentColor" fill="none" />
-        </svg>
-      );
-    case 'Circle':
-      return (
-        <svg width={size} height={size} viewBox="0 0 50 50">
-          <circle cx="25" cy="25" r="20" fill="currentColor" />
-        </svg>
-      );
-    default:
-      return null;
-  }
-};
-
 function App() {
+  const matches = [];
+  const cards = useRef<(HTMLElement | null)[]>([]);
   const [gridShapes, setGridShapes] = useState<Shape[]>([]);
+
+  function flipCard(index: number, shape: string) {
+    const card = cards.current[index];
+
+    if (!card) return;
+    card.classList.toggle('grid-item-turnover');
+
+    if (matches.length == 0) {
+      matches.push({ card: card, shape: shape });
+    } else {
+      if (matches[0].shape == shape);
+    }
+  }
+
+  const ShapeComponent = ({ name, cards, index }: { name: string; cards: React.MutableRefObject<(HTMLElement | null)[]>; index: number }) => {
+    const size = 75; // Size of the shape in pixels
+
+    const setRef = (elem: HTMLElement | null) => {
+      cards.current[index] = elem;
+    };
+
+    switch (name) {
+      case 'Triangle':
+        return (
+          <button type="button" className="grid-item-initial" ref={setRef} onClick={() => flipCard(index, name)}>
+            <svg width={size} height={size} viewBox="0 0 50 50">
+              <polygon points="25,5 45,45 5,45" stroke="currentColor" fill="none" />
+            </svg>
+          </button>
+        );
+      case 'Square':
+        return (
+          <button type="button" className="grid-item-initial" ref={setRef} onClick={() => flipCard(index, name)}>
+            <svg width={size} height={size} viewBox="0 0 60 60">
+              <rect x="5" y="5" width="50" height="50" stroke="currentColor" fill="none" />
+            </svg>
+          </button>
+        );
+      case 'Circle':
+        return (
+          <button type="button" className="grid-item-initial" ref={setRef} onClick={() => flipCard(index, name)}>
+            <svg width={size} height={size} viewBox="0 0 50 50">
+              <circle cx="25" cy="25" r="20" stroke="currentColor" fill="none" />
+            </svg>
+          </button>
+        );
+      default:
+        return null;
+    }
+  };
 
   useEffect(() => {
     // Create pairs of shapes to ensure matching is possible
@@ -48,7 +73,7 @@ function App() {
     <div className="grid-container">
       {gridShapes.map((shape, index) => (
         <div key={index} className="grid-item">
-          <ShapeComponent name={shape.name} />
+          <ShapeComponent name={shape.name} cards={cards} index={index} />
         </div>
       ))}
     </div>
