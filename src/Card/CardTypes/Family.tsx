ecd13b391471c86a713eb398a9b85af4d582b7ff
Michel Sterling <michel.sterling@gmail.com>
2025-11-24 08:05:55 -0600

    moved images

--- src/Card/CardTypes/Family.tsx
+++ src/Card/CardTypes/Family.tsx
@@ -1,7 +1,7 @@
 import styles from './Family.module.css';
 
 const Family = ({ familyMemberName }: { familyMemberName: string }) => {
-  return <img className={styles.image} src={`/src/assets/vector/${familyMemberName}-vector.png`} alt={familyMemberName} />;
+  return <img className={styles.image} src={`/assets/vector/${familyMemberName}-vector.png`} alt={familyMemberName} />;
 };
 
 export default Family;
