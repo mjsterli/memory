a1fa8abeacc3ac3368633f6eea0cd1bce455873c
Michel Sterling <michel.sterling@gmail.com>
2025-12-27 18:01:25 -0600

    fixed directory structure

--- src/Card/CardTypes/Family.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './Family.module.css';
-
-const Family = ({ familyMemberName }: { familyMemberName: string }) => {
-  return <img className={styles.image} src={`/assets/vector/${familyMemberName}-vector.png`} alt={familyMemberName} />;
-};
-
-export default Family;
