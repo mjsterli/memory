d22a76f35c25ea60be744f4b9598de32d01e1928
Michel Sterling Jr. <curtis.c.love@gmail.com>
2026-01-02 11:39:20 -0600

    added category picker

---
+++ src/app/component/category-picker/category-picker.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-picker',
+  imports: [],
+  templateUrl: './category-picker.html',
+  styleUrl: './category-picker.css',
+})
+export class CategoryPicker {
+
+}
